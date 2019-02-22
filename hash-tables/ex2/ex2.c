e89b015f161256fb9429cd387e25e5418c989069
Katie Cleary <katiecleary@airmaster.lan>
2019-02-22 13:52:21 -0500

    implemented code for ex2, not passing tests

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -9,34 +9,34 @@ char **reconstruct_trip(Ticket **tickets, int length)
   HashTable *ht = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
-  /* PSUEDOCODE / LOGIC 
-
-  // change value of NONE to 'start' and 'end' 
-  char route = [start] 
-
-  for (ticket in tickets) {
-    if ticket.source == NONE {
-      ticket.source = start
-    }
-    if ticket.destination == NONE {
-      ticket.destination = end 
-    }
-
+  /* PSUEDOCODE / LOGIC */
   //populate hashtable 
-  for (ticket in tickets) {
-    h[ticket.source] = ticket.destination // assign/insert source to keys, and destination to values 
+  for (int i = 0; i < length; i++) { // assign/insert source to keys, and destination to values 
+  hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
   }
 
-  current_city = start // set current city to start
-
-  while current_city != end { // as long as current city hasn't reached 'end', continue to loop 
-    current_ciy = h[current_city] 
-    append current_city to route  // fill in route with entry for each city visited
+  // change value of NONE to 'start' and 'end'
+  for (int i = 0; i < length; i++) {
+    if (tickets[i]->source == "NONE") {
+      tickets[i]->source = "START";
+    }
+    if (tickets[i]->destination == "NONE"){
+      tickets[i]->destination = "END";
+    }
   }
-  current_city = end // set current city to end
-  append current_city to route // 
 
- */
+  char* current_city = tickets[0]->source; // set current city to start
+ 
+  while(current_city != "END") { // as long as current city hasn't reached 'end', continue to loop 
+    current_city = hash_table_retrieve(ht,current_city);
+    for (int i = 0; i < length; i++) { // fill in route with entry for each city visited
+    route[i] = hash_table_retrieve(ht,current_city); 
+    } 
+  }
+
+  current_city = "END"; // set current city to end
+  //append current_city to route // 
+
   return route;
 }
 
@@ -56,7 +56,7 @@ int main(void)
   Ticket **tickets = malloc(3 * sizeof(Ticket *));
 
   Ticket *ticket_1 = malloc(sizeof(Ticket));
-  ticket_1->source = "NONE";
+  ticket_1->source = "START";
   ticket_1->destination = "PDX";
   tickets[0] = ticket_1;
 
@@ -67,7 +67,7 @@ int main(void)
 
   Ticket *ticket_3 = malloc(sizeof(Ticket));
   ticket_3->source = "DCA";
-  ticket_3->destination = "NONE";
+  ticket_3->destination = "END";
   tickets[2] = ticket_3;
 
   print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
@@ -75,3 +75,6 @@ int main(void)
   return 0;
 }
 #endif
+
+
+ 
